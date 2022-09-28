ca0255100bb4c225c38c0d8623b2f7e9b1dc2b33
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-09-28 08:48:40 +0330

    fix:(Controled) deleting unnecessary looping

--- pagination-app/src/components/Lists/Controled.tsx
+++ pagination-app/src/components/Lists/Controled.tsx
@@ -2,25 +2,16 @@ import { UserDataType, UsersListType } from "../../types/types";
 import "./list.css";
 
 export default function List(props: UsersListType) {
-  let { data, listItem: ListItem, step, perPageCount = 12 } = props;
+  let { data, listItem: ListItem, perPageCount = 12 } = props;
   return (
     <ul className="flex">
       {data.length > 0
-        ? data.map((user: UserDataType) => {
-            console.log(user);
-            while (step > 0) {
-              step--;
-              return <ListItem key={user.id} {...user}></ListItem>;
-            }
-          })
+        ? data.map((user: UserDataType) => (
+            <ListItem key={user.id} {...user}></ListItem>
+          ))
         : Array(perPageCount) //for loading state
             .fill(1)
-            .map((_, idx: number) => {
-              while (step > 0) {
-                step--;
-                return <ListItem key={idx}></ListItem>;
-              }
-            })}
+            .map((_, idx: number) => <ListItem key={idx}></ListItem>)}
     </ul>
   );
 }
