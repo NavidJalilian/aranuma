9a1b7c56302881bbf8ab238e8e696707262a878d
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-09-28 09:22:46 +0330

    refactor:(Regular) change func name

--- pagination-app/src/components/Lists/Regular.tsx
+++ pagination-app/src/components/Lists/Regular.tsx
@@ -1,7 +1,7 @@
 import { UserDataType, UsersListType } from "../../types/types";
 import "./Regular.css";
 
-export default function List(props: UsersListType) {
+export default function Regular(props: UsersListType) {
   const { data, listItem: ListItem, perPageCount = 12 } = props;
   return (
     <ul className="flex">
