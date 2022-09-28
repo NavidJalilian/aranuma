1de83d423260b91d78618746559820a2397bd3d1
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-09-28 09:01:36 +0330

    refactor:(App) change import name

--- pagination-app/src/App.tsx
+++ pagination-app/src/App.tsx
@@ -1,11 +1,11 @@
 import UserStatusProvider from "./contexts/UserStatusContext";
-import UsersPage from "./pages/PaginationPage";
+import PaginationPage from "./pages/PaginationPage";
 
 function App() {
   return (
     <div className="App">
       <UserStatusProvider>
-        <UsersPage />
+        <PaginationPage />
       </UserStatusProvider>
     </div>
   );
